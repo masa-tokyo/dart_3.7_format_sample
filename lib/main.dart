f23fe9900f03898c6bd939ced6ebda4c80d70903
masaki <masakisato.japan@gmail.com>
2025-02-17 16:19:44 +0900

    add functions for named and positional arguments

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    namedArgFunc(callback: () => null, keys: []);
+
+    positionalArgFunc(() => null, []);
+
     return const MaterialApp(
       home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
+}
+
+void namedArgFunc({
+  required Object? Function() callback,
+  required List<Object> keys,
+}) {
+  callback();
+}
+
+void positionalArgFunc(Object? Function() callback, List<Object> keys) {
+  callback();
 }
