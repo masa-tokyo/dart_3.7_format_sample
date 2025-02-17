5ed67e4385484e0c25a50e3a819bbae5fb584075
masaki <masakisato.japan@gmail.com>
2025-02-17 16:26:42 +0900

    add block body functions

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  namedArgFunc(callback: () => null, keys: []);
+  namedArgFunc(
+    callback: () {
+      return null;
+    },
+    keys: [],
+  );
+
+  positionalArgFunc(() => null, []);
+  positionalArgFunc(() {
+    return null;
+    // `require_trailing_commas` warns here:
+    // https://dart.dev/tools/linter-rules/require_trailing_commas
+  }, []);
+
   runApp(const MainApp());
 }
 
@@ -9,23 +24,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    namedArgFunc(callback: () => null, keys: []);
-
-    positionalArgFunc(() => null, []);
-
     return const MaterialApp(
       home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
 
-void namedArgFunc({
-  required Object? Function() callback,
-  required List<Object> keys,
-}) {
-  callback();
+void namedArgFunc({Object? Function()? callback, List<Object>? keys}) {
+  callback?.call();
 }
 
-void positionalArgFunc(Object? Function() callback, List<Object> keys) {
-  callback();
+void positionalArgFunc(Object? Function()? callback, List<Object>? keys) {
+  callback?.call();
 }
