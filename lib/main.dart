ea0630d6b2b927daa5fe73ee1902df1a04ed55c8
masaki <masakisato.japan@gmail.com>
2025-02-18 10:35:48 +0900

    remove unnecessary function
    - back to 80 page_width
    - only having named function was enough

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 
 void namedParamsFuncTwoLists({List<String>? list1, List<String>? list2}) {}
 
-void namedParamsFuncListThenString({List<String>? list, String? string}) {}
-
 void positionalParamsFuncTwoLists(List<String>? list1, List<String>? list2) {}
 
 void positionalParamsFuncListThenString(List<String>? list, String? string) {}
@@ -15,21 +13,18 @@ void positionalParamsTwoStrings(String? string1, String? string2) {}
 void main() {
   namedParamsFuncTwoLists(list1: [], list2: []);
   namedParamsFuncTwoLists(
-    list1: ['This is a very long string that exceeds the 80 character limit as a list item.'],
+    list1: [
+      'This is a very long string that exceeds the 80 character limit as a list item.',
+    ],
     list2: [],
-  );
-
-  namedParamsFuncListThenString(
-    list: ['This is a very long string that exceeds the 80 character limit as a list item.'],
-    string: '',
   );
 
   positionalParamsFuncTwoLists([], []);
 
-  // no warning
-  positionalParamsFuncTwoLists(['This is a very long string that exceeds the 80 character limit as a list item.'], []);
-
-  positionalParamsFuncListThenString([], '');
+  positionalParamsFuncTwoLists([
+    'This is a very long string that exceeds the 80 character limit as a list item.',
+    // `require_trailing_commas` warns here
+  ], []);
 
   positionalParamsFuncListThenString([
     'This is a very long string that exceeds the 80 character limit as a list item.',
@@ -41,7 +36,10 @@ void main() {
     [],
   );
 
-  positionalParamsTwoStrings('This is a very long string that exceeds the 80 character limit as a list item.', '');
+  positionalParamsTwoStrings(
+    'This is a very long string that exceeds the 80 character limit as a list item.',
+    '',
+  );
 
   runApp(const MainApp());
 }
@@ -51,6 +49,8 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Scaffold(body: Center(child: Text('Hello World!'))));
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    );
   }
 }
